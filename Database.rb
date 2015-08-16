6cafd29052d2b3f59e8a7e148ea8ae4964d8c406
valentinerutto <rutto28@gmail.com>
2015-08-16 17:56:39 +0300

    sql definition

--- Database.rb
+++ Database.rb
@@ -1,2 +1,7 @@
+///////////////DATABASE/////////
 database collect and organize data and information for easy retrieval
-    the information is held in tables
+    DB consists of tables with rows and columns where the actual information is stored
+   ///////// SQL/////
+    its a proglanguage that allows interaction with the databases
+    written as statements that asks the db fr sm results
+    
