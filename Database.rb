64874db7ad204f3bed1536e0b6f2f0910a635961
valentinerutto <rutto28@gmail.com>
2015-08-16 18:02:09 +0300

    retrieving data using select command

--- Database.rb
+++ Database.rb
@@ -4,4 +4,13 @@ database collect and organize data and information for easy retrieval
    ///////// SQL/////
     its a proglanguage that allows interaction with the databases
     written as statements that asks the db fr sm results
-    
+    --->retrive data
+    :select
+    this retrives data from a specific table and colum
+    syntax:
+    SELECT-----
+        FROM------;
+    SELECT title,genre//colums
+    FROM movies;//table
+    SELECT *
+        FROM movies; //selects all matching results in table 
